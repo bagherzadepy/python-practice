8a337754d0b8a704824e5a08f01fef0d199b7954
Amir403py <helloamirbye@gmail.com>
2025-08-26 15:13:33 +0330

    Day 1: add document and comment

--- day1.py
+++ day1.py
@@ -1,18 +1,45 @@
+# Day 1 - Python Basics Practice
+# Developer: Amir
+# Comment: Python Basic Practice (list, dict, function)
 
+# -----------------------------------------------------
+# تمرین 1: کار با لیست قیمت کالاها
+# -----------------------------------------------------
 
 prices = [12000, 48000, 284000, 18000, 108000, 34000]
-total_prices = sum(prices)
-average_prices = total_prices / len(prices)
-max_price = max(prices)
+total_prices = sum(prices) # جمع
+average_prices = total_prices / len(prices) # میانگین
+max_price = max(prices) # بیشترین قیمت
+
+print("Prices:", prices)
+print("Total:", total_prices)
+print("Maximum Prices:", max_price)
+
+
+# -----------------------------------------------------
+# تمرین 2: دیکشنری اطلاعات شخصی
+# -----------------------------------------------------
 
 person = {
     "name": "Amir",
     "age": 27,
-    "accupation": "developer",
+    "job": "Developer",
     "city": "Tehran"
 }
-person["age"] = 37
 
-def max_numbers(*numbers):
+print("\n Initialize Information:", person)
+
+person["age"] = 37 # تغییر مقدار
+print("\n Information after change:", person)
+
+
+# -----------------------------------------------------
+# تمرین 3: تابع پیدا کردن بزرگترین عدد
+# -----------------------------------------------------
+
+def find_max(*numbers):
+    """ تابعی که بزرگترین عدد بین اعداد ارسالی را برمی گرداند"""
     return max(numbers)
-print(max_numbers(13, 29, 14, 26))
+
+
+print("\n Maximum Number:", find_max(13, 29, 14, 26))
